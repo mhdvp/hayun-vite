e68d74e4fab7c47065d2d1e49f4926f14dfb89d4
Mahdi Javidi <daneshkav@yahoo.com>
2025-02-09 11:48:12 +0330

    0.0.1

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import AudiogramChart from './src/Audiogram/Audiogram'
 import dims from './src/Audiogram/dims'
 import logo from './public/vite.svg'
 import { officeData, patientData } from './data/sampleData'
+import Forms from './src/Form/Forms'
 
 
 
 
-export { logo, AudiogramChart, dims, officeData, patientData } 
+
+export { logo, AudiogramChart,Forms, dims, officeData, patientData } 
